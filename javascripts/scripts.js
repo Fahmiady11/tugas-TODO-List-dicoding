1fcfb7364692e939651bec2e096b4555943c7cdf
fahmiady11 <asfahmi5@gmail.com>
2021-06-18 15:16:45 +0700

    tugas

---
+++ javascripts/scripts.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", function () {
+ 
+    const submitForm = document.getElementById("form");
+ 
+    submitForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        addTodo();
+    });
+    if(isStorageExist()){
+        loadDataFromStorage();
+    }
+});
+document.addEventListener("ondatasaved",()=>{
+    console.log("data berhasil disimpan");
+});
+document.addEventListener("ondataloaded",()=>{
+    refreshDataFromTodos();
+});
